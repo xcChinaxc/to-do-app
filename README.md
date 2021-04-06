211e9097bbbf0b20b60da88fe90c74379decf318
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-04-06 13:12:08 -0400

    merging with local branch

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# todoapp
+# My To Do App
+This is a very simple to-do application. It's still a work in progress!
+
+## How It's Made
+**Tech used:** HTML, CSS, JavaScript, Embedded JavaScript, Node.js, Express, MongoDB
--- server.js
+++ server.js
@@ -1,46 +1,74 @@
-const express = require('express');
-const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const PORT = process.env.PORT || 5000;
-require('dotenv').config({ path: '.env'})
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
 
 let db,
 dbConnectionStr = process.env.DB_STRING,
-dbName = 'to-do-list';
+dbName = 'to-do-list'
+const PORT = process.env.PORT
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
   .then(client => {
-    console.log(`Connected to ${dbName} Database`);
-    db = client.db(dbName);
-    const tasksCollection = db.collection('tasks');
-  });
-    
- app.set('view engine', 'ejs')
- app.use(express.static('public'))
- app.use(express.urlencoded({ extended: true }))
- app.use(express.json())
+    console.log(`Connected to ${dbName} Database`)
+    db = client.db(dbName)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
- app.get('/', (req, res) => {
-   db.collection('tasks').find().toArray()
-   .then(results => {
-     res.render('index.ejs', {tasks: results });
-    })
-   .catch()
+  app.set('view engine', 'ejs')
+  app.use(express.static('public'))
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  
+  app.get('/', async (req, res) => {
+    const todoItems = await db.collection('todos').find().toArray()
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    res.render('index.ejs', {tasks: todoItems, left: itemsLeft})
  })
 
- app.post('/taskItem', (req, res) => {
-   tasksCollection.insertOne(req.body)
+ app.post('/createTodo', (req, res) => {
+   db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
    .then(result => {
+     console.log('Todo has been added!')
      res.redirect('/')
-     console.log(result);
+   })
+ })
+
+ app.put('/markComplete', (req, res) => {
+   db.collection('todos').updateOne({todo: req.body.taskName}, {
+     $set: {
+       completed: true
+     }
+  })
+  .then(result => {
+    console.log('Todo has been marked complete!')
+    res.json('Marked complete!')
+  })
+ })
+
+ app.put('/undo', (req, res) => {
+  db.collection('todos').updateOne({todo: req.body.taskName}, {
+    $set: {
+      completed: false
+    }
+ })
+ .then(result => {
+   console.log('Todo has been re-added!')
+   res.json('Marked not complete!')
+ })
+})
+
+ app.delete('/deleteTodo', (req, res)=> {
+   db.collection('todos').deleteOne({todo: req.body.taskName})
+   .then(result => {
+     console.log('Todo has been deleted!')
+     res.json('Deleted it')
    })
    .catch(err => console.log(err))
  })
 
- app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`);
- });
-
-
-
-
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running!`)
+  });
